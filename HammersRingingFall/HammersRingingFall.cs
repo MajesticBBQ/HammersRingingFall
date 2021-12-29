5d9d5e8780a843210454d80fc391f05d53c2fd5c
VinterNacht <crannnabeatha@gmail.com>
2021-12-29 08:20:39 -0600

    Add project files.

---
+++ HammersRingingFall/HammersRingingFall.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Vintagestory.API.Common;
+
+namespace HammersRingingFall
+{
+    public class HammersRingingFall : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+        }
+    }
+}
