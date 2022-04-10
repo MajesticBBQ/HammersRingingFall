a8d1007fe0bc5079ee1ff7b61b2a7991f8df173d
VinterNacht <crannnabeatha@gmail.com>
2022-04-10 10:58:27 -0500

    Updating compatibility

--- HammersRingingFall/HammersRingingFall.cs
+++ HammersRingingFall/HammersRingingFall.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using HammersRingingFall;
+using System;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 
 namespace HammersRingingFall
 {
     public class HammersRingingFall : ModSystem
     {
+        public override bool ShouldLoad(EnumAppSide forSide)
+        {
+            return true;
+        }
+
         public override void Start(ICoreAPI api)
         {
             base.Start(api);
+            api.RegisterEntity("EntityDrifter", typeof(EntityDrifter));
         }
     }
+
+    
 }
